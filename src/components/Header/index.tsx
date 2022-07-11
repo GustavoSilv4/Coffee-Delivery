e651a50b1fb08f212cf64fa60978e2f9b61a2b30
GustavoSilv4 <GustavoSilv4.contato@gmail.com>
2022-07-11 18:19:59 -0300

    feat: separating the pages and creating the header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logoCoffee from './assets/logo-coffee.svg'
+
+export function Header() {
+  return (
+    <div>
+      <img src={logoCoffee} alt="" />
+      <div>
+        <span>Porto Alegre, RS</span>
+        <span>Carrinho</span>
+      </div>
+    </div>
+  )
+}
