3b1c3b68eb8c8f792adba83791fd1472e688a3ad
GustavoSilv4 <GustavoSilv4.contato@gmail.com>
2022-07-11 18:08:58 -0300

    feat: created global style and page theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import App from './App'
 
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={defaultTheme}>
+    <React.StrictMode>
+      <App />
+      <GlobalStyle />
+    </React.StrictMode>
+  </ThemeProvider>
 )
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  'white-100': '#ffffff',
+  'white-200': '#FAFAFA',
+
+  'gray-100': '#F3F2F2',
+  'gray-200': '#EDEDED',
+  'gray-300': '#E6E5E5',
+  'gray-400': '#D7D5D5',
+
+  'brown-600': '#8D8686',
+  'brown-700': '#574F4D',
+  'brown-800': '#403937',
+  'brown-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-900': '#4B2995',
+
+  'orange-100': '#F1E9C9',
+  'orange-500': '#DBAC2C',
+  'orange-900': '#C47F17',
+}
