8c1fcb6d12e6d1dd0aa8eec823e278dcd6e347d4
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-14 17:19:02 -0300

    feat: header receives location props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,18 @@ import { HeaderContainer, InfoContainer } from './styles'
 import logoCoffee from '../../assets/logo-coffee.svg'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 
-export function Header() {
+interface HeaderProps {
+  location?: string
+}
+
+export function Header({ location = 'A definir' }: HeaderProps) {
   return (
     <HeaderContainer>
       <img src={logoCoffee} alt="" />
       <InfoContainer>
         <span>
           <MapPin size={22} color="#8047F8" weight="fill" />
-          Porto Alegre, RS
+          {location}
         </span>
         <span>
           <ShoppingCart size={22} weight="fill" />
--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -4,7 +4,7 @@ import { Header } from '../../components/Header'
 export function DefaultLayout() {
   return (
     <>
-      <Header />
+      <Header location="Rio de Janeiro, RJ" />
       <Outlet />
     </>
   )
