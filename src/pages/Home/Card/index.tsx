987c4a33837f1a69de28976190da2f1abeae3bf2
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-14 18:51:24 -0300

    feat: completed button functions referring to input quantity and conditional rendering of types

--- src/pages/Home/Card/index.tsx
+++ src/pages/Home/Card/index.tsx
@@ -1,9 +1,11 @@
+import { ChangeEvent, useState } from 'react'
+
 import { Minus, Plus, ShoppingCart } from 'phosphor-react'
 import {
   ButtonCart,
   CardContainer,
+  CoffeeTypes,
   Description,
-  InputCard,
   MinusButton,
   Name,
   PlusButton,
@@ -12,29 +14,67 @@ import {
   SelectContainer,
   Type,
 } from './styles'
-import Americano from '../../../assets/Products/Americano.png'
-import { useState } from 'react'
 
-export function Card() {
+interface TypesCoffee {
+  traditional?: boolean
+  iced?: boolean
+  withMilk?: boolean
+  alcoholic?: boolean
+  special?: boolean
+}
+interface CardProps {
+  image: string
+  name: string
+  description: string
+  types: TypesCoffee
+}
+
+export function Card({ name, description, image, types }: CardProps) {
   const [quantity, setQuantity] = useState(0)
+
+  const handleChangeQuantity = (event: ChangeEvent<HTMLInputElement>) => {
+    const newQuantity = Number(event.target.value)
+    setQuantity(newQuantity)
+  }
+
+  const handleMinusQuantity = () => {
+    if (quantity > 0) {
+      setQuantity((state) => state - 1)
+    }
+  }
+
+  const handlePlusQuantity = () => {
+    if (quantity < 10) {
+      setQuantity((state) => state + 1)
+    }
+  }
 
   return (
     <CardContainer>
-      <img src={Americano} alt="" />
-      <Type>TRADICIONAL</Type>
-      <Name>Expresso Tradicional</Name>
-      <Description>
-        O tradicional café feito com água quente e grãos moídos
-      </Description>
+      <img src={image} alt="" />
+      <CoffeeTypes>
+        {types.traditional && <Type>TRADICIONAL</Type>}
+        {types.iced && <Type>GELADO</Type>}
+        {types.withMilk && <Type>COM LEITE</Type>}
+        {types.alcoholic && <Type>ALCOÓLICO</Type>}
+        {types.special && <Type>ESPECIAL</Type>}
+      </CoffeeTypes>
+      <Name>{name}</Name>
+      <Description>{description}</Description>
       <SelectContainer>
         <Price>
           <span>R$</span>9,90
         </Price>
-        <MinusButton>
+        <MinusButton onClick={handleMinusQuantity}>
           <Minus size={14} weight="bold" />
         </MinusButton>
-        <QuantityInput type="number" value={quantity} />
-        <PlusButton>
+        <QuantityInput
+          type="number"
+          value={String(quantity)}
+          max={10}
+          onChange={handleChangeQuantity}
+        />
+        <PlusButton onClick={handlePlusQuantity}>
           <Plus size={14} weight="bold" />
         </PlusButton>
         <ButtonCart>
