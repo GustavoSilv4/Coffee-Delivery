96f8602206f7891a679fd0b11e6a6c8d8e793499
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-15 10:08:01 -0300

    feat: receives the amount of products per prop and does a conditional rendering of the style

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface ButtonProps {
+  quantityProduct: number
+}
+
 export const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
@@ -13,23 +17,45 @@ export const Location = styled.span`
   font-size: 0.875rem;
   line-height: 1.3;
 
-  background-color: ${(props) => props.theme['purple-100']};
-  color: ${(props) => props.theme['purple-900']};
+  background-color: ${(props) => props.theme['purple-light']};
+  color: ${(props) => props.theme['purple-dark']};
 
   display: flex;
   align-items: center;
   gap: 0.25rem;
 `
 
-export const ButtonCart = styled.span`
+export const ButtonCart = styled.span<ButtonProps>`
   padding: 0.5rem;
   border-radius: 6px;
 
-  background-color: ${(props) => props.theme['yellow-100']};
-  color: ${(props) => props.theme['yellow-900']};
+  background-color: ${(props) => props.theme['yellow-light']};
+  color: ${(props) => props.theme['yellow-dark']};
 
   display: flex;
   align-items: center;
+
+  position: relative;
+
+  &::after {
+    position: absolute;
+    border-radius: 100%;
+    font-size: 0.75rem;
+    font-weight: bold;
+    text-align: center;
+
+    left: 1.75rem;
+    bottom: 1.6rem;
+
+    width: 1.25rem;
+    height: 1.25rem;
+
+    background-color: ${(props) => props.theme['yellow-dark']};
+    color: ${(props) => props.theme.white};
+
+    content: '${(props) => String(props.quantityProduct)}';
+    display: ${(props) => (props.quantityProduct > 0 ? 'inline' : 'none')};
+  }
 `
 
 export const InfoContainer = styled.div`
