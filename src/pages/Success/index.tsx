c8c6d1b8fa5368e15104471f7f6e95400087c535
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-16 20:02:06 -0300

    feat: created HTML, CSS of the page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,57 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+
+import Ilustracao from '../../assets/Illustration.svg'
+import {
+  BoxInfo,
+  BoxTitleAndSubtitle,
+  DollarIcon,
+  Info,
+  MapIcon,
+  SuccessContainer,
+  TimerIcon,
+} from './styles'
+
 export function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <BoxTitleAndSubtitle>
+        <h1>Uhu! Pedido confirmado</h1>
+        <h3>Agora é só aguardar que logo o café chegará até você</h3>
+      </BoxTitleAndSubtitle>
+      <BoxInfo>
+        <Info>
+          <div>
+            <MapIcon>
+              <MapPin size={16} weight="fill" color="white" />
+            </MapIcon>
+            <div>
+              <h5>
+                Entrega em <span>Rua João Daniel Martinelli, 102</span>
+              </h5>
+              <h5>Farrapos - Porto Alegre, RS</h5>
+            </div>
+          </div>
+          <div>
+            <TimerIcon>
+              <Timer size={16} weight="fill" color="white" />
+            </TimerIcon>
+            <div>
+              <h5>Previsão de entrega</h5>
+              <span>20 min - 30 min</span>
+            </div>
+          </div>
+          <div>
+            <DollarIcon>
+              <CurrencyDollar size={16} weight="fill" color="white" />
+            </DollarIcon>
+            <div>
+              <h5>Previsão de entrega</h5>
+              <span>20 min - 30 min </span>
+            </div>
+          </div>
+        </Info>
+        <img src={Ilustracao} alt="" />
+      </BoxInfo>
+    </SuccessContainer>
+  )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,96 @@
+import styled from 'styled-components'
+
+export const SuccessContainer = styled.main`
+  margin: 0rem 10rem;
+`
+
+export const BoxTitleAndSubtitle = styled.div`
+  margin-bottom: 2.5rem;
+  h1 {
+    font-family: 'Baloo 2', cursive;
+    font-size: 2rem;
+    font-weight: bold;
+    color: ${(props) => props.theme['yellow-dark']};
+    line-height: 1.3;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+    font-weight: normal;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+
+export const BoxInfo = styled.div`
+  display: flex;
+  gap: 6.375rem;
+`
+
+export const Info = styled.div`
+  background: linear-gradient(
+        ${(props) => props.theme.background},
+        ${(props) => props.theme.background}
+      )
+      padding-box,
+    linear-gradient(to right, yellow, purple) border-box;
+  border: 1px solid transparent;
+  border-radius: 6px 36px 6px 36px;
+  padding: 2.5rem;
+
+  > div:nth-child(1) {
+    padding-bottom: 2rem;
+  }
+
+  > div:nth-child(2) {
+    padding-bottom: 2rem;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    margin-right: 8.25rem;
+
+    > div {
+      h5 {
+        font-size: 1rem;
+        font-weight: normal;
+        line-height: 1.3;
+        color: ${(props) => props.theme['base-text']};
+
+        span {
+          // Dentro do H5
+          font-weight: bold;
+        }
+      }
+      span {
+        // Fora do H5
+        color: ${(props) => props.theme['base-text']};
+        font-weight: bold;
+      }
+    }
+  }
+`
+
+const BaseIcons = styled.span`
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-right: 0.75rem;
+
+  width: 2rem;
+  height: 2rem;
+`
+
+export const MapIcon = styled(BaseIcons)`
+  background-color: ${(props) => props.theme.purple};
+`
+
+export const TimerIcon = styled(BaseIcons)`
+  background-color: ${(props) => props.theme.yellow};
+`
+
+export const DollarIcon = styled(BaseIcons)`
+  background-color: ${(props) => props.theme['yellow-dark']};
+`
