ac1d753ce146e76661f3a7e0bbeb16aeee71644d
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-18 15:53:40 -0300

    feat: created order storage array and its handling functions

--- src/contexts/OrderContext.tsx
+++ src/contexts/OrderContext.tsx
@@ -1,11 +1,54 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
-export const OrderContext = createContext({})
+export interface OrderProps {
+  id: number
+  name: string
+  quantity: number
+}
+
+interface ChangeOrderQuantity {
+  id: number
+  newQuantity: number
+}
+interface OrderContextType {
+  createNewOrder: (data: OrderProps) => void
+  orders: OrderProps[]
+  changeQuantityProduct: (data: ChangeOrderQuantity) => void
+}
+
+export const OrderContext = createContext({} as OrderContextType)
 
 interface OrderContextProviderProps {
   children: ReactNode
 }
 
 export function OrderContextProvider({ children }: OrderContextProviderProps) {
-  return <OrderContext.Provider value={{}}>{children}</OrderContext.Provider>
+  const [orders, setOrders] = useState<OrderProps[]>([])
+
+  const createNewOrder = (data: OrderProps) => {
+    const newOrder = {
+      id: data.id,
+      name: data.name,
+      quantity: data.quantity,
+    }
+
+    setOrders((state) => [...state, newOrder])
+  }
+
+  const changeQuantityProduct = ({ newQuantity, id }: ChangeOrderQuantity) => {
+    setOrders((prevOrder) =>
+      prevOrder.map((order) =>
+        order.id === id ? { ...order, quantity: newQuantity } : order
+      )
+    )
+  }
+
+  console.log(orders)
+
+  return (
+    <OrderContext.Provider
+      value={{ createNewOrder, changeQuantityProduct, orders }}>
+      {children}
+    </OrderContext.Provider>
+  )
 }
