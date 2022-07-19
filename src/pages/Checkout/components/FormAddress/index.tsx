ac665844e7f05cb9fd0c14c066f180a35237fa99
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-18 21:43:05 -0300

    feat: separate the form address in another component

---
+++ src/pages/Checkout/components/FormAddress/index.tsx
@@ -0,0 +1,160 @@
+import { useContext, useState } from 'react'
+import { OrderContext } from '../../../../contexts/OrderContext'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+import {
+  BoxForm,
+  BoxFormSubtitle,
+  BoxPayment,
+  BoxPaymentSubtitle,
+  ButtonsPaymentContainer,
+  Credit,
+  Debit,
+  Form,
+  InputBairro,
+  InputCEP,
+  InputCidade,
+  InputComplemento,
+  InputNum,
+  InputRua,
+  InputUF,
+  MoneyPay,
+} from './styles'
+
+const addressFormValidationSchema = zod.object({
+  // Aqui crio o esquema de validacao do form
+  cep: zod
+    .string()
+    .regex(/\d{2}.\d{3}-\d{3}/, 'Informe o CEP no formato correto!')
+    .max(10)
+    .min(10),
+  rua: zod.string().max(50).min(1, 'Informe corretamento o nome da rua!'),
+  numero: zod.string().min(1, 'Informe corretamento o numero da residência'),
+  complemento: zod.string().max(50).optional(),
+  bairro: zod.string().max(50).min(1, 'Informe corretamento o nome do bairro!'),
+  cidade: zod.string().max(50).min(1, 'Informe corretamento o nome da cidade!'),
+  UF: zod.string().max(2).min(2, 'Informe corretamento o UF!'),
+})
+
+type AddressFormData = zod.infer<typeof addressFormValidationSchema> // Aqui infiro(automatizo) a tipagem do formulario através do objeto de validacao
+
+export function FormAddress() {
+  const { registerAddress, registerPaymentType } = useContext(OrderContext)
+
+  const { register, reset, handleSubmit } = useForm<AddressFormData>({
+    resolver: zodResolver(addressFormValidationSchema), // Passo o esquema de validacao para o resolver -> resolver do zod
+    defaultValues: {
+      bairro: '',
+      cep: '',
+      cidade: '',
+      complemento: '',
+      numero: '',
+      rua: '',
+      UF: '',
+    },
+  })
+
+  // console.log(formState.errors) // Validacao das mensagens erros
+  // watch('cep') // Vigia se houve alteracao no input
+
+  const [paymentType, setPaymentType] = useState('')
+
+  const handleRegisterToDelivery = (data: AddressFormData) => {
+    if (paymentType !== '') {
+      registerAddress(data)
+      registerPaymentType(paymentType)
+      reset() // Reseta o formulario após ser submetido
+      setPaymentType('')
+    }
+  }
+
+  return (
+    <>
+      <BoxForm>
+        <BoxFormSubtitle>
+          <MapPinLine size={22} />
+          <div>
+            <h4>Endereco de Entrega</h4>
+            <span>Informe o endereço onde deseja receber seu pedido</span>
+          </div>
+        </BoxFormSubtitle>
+        <Form
+          onSubmit={handleSubmit(handleRegisterToDelivery)}
+          id="form-delivery">
+          <InputCEP
+            type="text"
+            maxLength={10}
+            placeholder="CEP"
+            {...register('cep')}
+          />
+          <InputRua type="text" placeholder="Rua" {...register('rua')} />
+          <div>
+            <InputNum
+              type="number"
+              placeholder="Número"
+              {...register('numero')}
+            />
+            <InputComplemento
+              type="text"
+              placeholder="Complemento"
+              {...register('complemento')}
+            />
+          </div>
+          <div>
+            <InputBairro
+              type="text"
+              placeholder="Bairro"
+              {...register('bairro')}
+            />
+            <InputCidade
+              type="text"
+              placeholder="Cidade"
+              {...register('cidade')}
+            />
+            <InputUF type="text" placeholder="UF" {...register('UF')} />
+          </div>
+        </Form>
+      </BoxForm>
+      <BoxPayment>
+        <BoxPaymentSubtitle>
+          <CurrencyDollar size={22} />
+          <div>
+            <h4>Pagamento</h4>
+            <span>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </span>
+          </div>
+        </BoxPaymentSubtitle>
+        <ButtonsPaymentContainer>
+          <Credit
+            onClick={(e) => setPaymentType('Credit')}
+            paymentType={paymentType}>
+            <CreditCard size={16} color="#8047F8" />
+            CARTÃO DE CRÉDITO
+          </Credit>
+          <Debit
+            onClick={(e) => setPaymentType('Debit')}
+            paymentType={paymentType}>
+            <Bank size={16} color="#8047F8" />
+            CARTÃO DE DÉBITO
+          </Debit>
+          <MoneyPay
+            onClick={(e) => setPaymentType('Money')}
+            paymentType={paymentType}>
+            <Money size={16} color="#8047F8" />
+            DINHEIRO
+          </MoneyPay>
+        </ButtonsPaymentContainer>
+      </BoxPayment>
+    </>
+  )
+}
---
+++ src/pages/Checkout/components/FormAddress/styles.ts
@@ -0,0 +1,169 @@
+import styled from 'styled-components'
+
+interface PaymentTypeProps {
+  paymentType: string
+}
+
+export const BoxForm = styled.div`
+  padding: 2.5rem;
+  background-color: ${(props) => props.theme['base-card']};
+  border-radius: 6px;
+  margin-bottom: 0.75rem;
+`
+
+export const BoxFormSubtitle = styled.div`
+  height: 2.75rem;
+  display: flex;
+  gap: 0.5rem;
+  margin-bottom: 2rem;
+
+  div {
+    h4 {
+      font-size: 1rem;
+      font-weight: normal;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    span {
+      font-size: 0.875rem;
+      font-family: inherit;
+      color: ${(props) => props.theme['base-text']};
+    }
+  }
+
+  color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const BaseInputForm = styled.input`
+  padding: 0.76rem;
+  border: 1px solid ${(props) => props.theme['base-button']};
+  border-radius: 4px;
+  margin-bottom: 1rem;
+  font-size: 0.875rem;
+
+  color: ${(props) => props.theme['base-text']};
+  background-color: ${(props) => props.theme['base-input']};
+
+  &:focus {
+    outline: 1px solid ${(props) => props.theme['yellow-dark']};
+  }
+`
+export const InputCEP = styled(BaseInputForm)`
+  width: 12.5rem;
+`
+
+export const InputRua = styled(BaseInputForm)``
+
+export const InputNum = styled(BaseInputForm)`
+  width: 12.5rem;
+  margin-right: 0.75rem;
+`
+
+export const InputComplemento = styled(BaseInputForm)`
+  width: 22rem;
+`
+
+export const InputBairro = styled(BaseInputForm)`
+  width: 12.5rem;
+  margin-right: 0.75rem;
+`
+
+export const InputCidade = styled(BaseInputForm)`
+  width: 17.5rem;
+  margin-right: 0.75rem;
+`
+
+export const InputUF = styled(BaseInputForm)`
+  width: 3.75rem;
+`
+
+export const BoxPayment = styled.div`
+  padding: 2.5rem;
+  background-color: ${(props) => props.theme['base-card']};
+  border-radius: 6px;
+`
+export const BoxPaymentSubtitle = styled.div`
+  height: 2.75rem;
+  display: flex;
+  gap: 0.5rem;
+  margin-bottom: 2rem;
+
+  div {
+    h4 {
+      font-size: 1rem;
+      font-weight: normal;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    span {
+      font-size: 0.875rem;
+      font-family: inherit;
+      color: ${(props) => props.theme['base-text']};
+    }
+  }
+  color: ${(props) => props.theme.purple};
+`
+
+export const ButtonsPaymentContainer = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+export const ButtonPayment = styled.button`
+  width: 11.125rem;
+  font-size: 0.75rem;
+  line-height: 1.6;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+
+  padding: 1rem 0rem;
+  padding-left: 1rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 0.75rem;
+
+  background-color: ${(props) => props.theme['base-button']};
+  color: ${(props) => props.theme['base-text']};
+
+  transition: background-color 0.1s;
+`
+
+export const Credit = styled(ButtonPayment)<PaymentTypeProps>`
+  &:hover:not(:active) {
+    background-color: ${(props) => props.theme['base-hover']};
+  }
+
+  ${(props) =>
+    props.paymentType === 'Credit' ? 'outline: 1px solid #8047F8' : 'none'};
+  ${(props) =>
+    props.paymentType === 'Credit' ? 'background-color: #EBE5F9' : 'none'};
+`
+
+export const Debit = styled(ButtonPayment)<PaymentTypeProps>`
+  &:hover:not(:active) {
+    background-color: ${(props) => props.theme['base-hover']};
+  }
+
+  ${(props) =>
+    props.paymentType === 'Debit' ? 'outline: 1px solid #8047F8' : 'none'};
+  ${(props) =>
+    props.paymentType === 'Debit' ? 'background-color: #EBE5F9' : 'none'};
+`
+
+export const MoneyPay = styled(ButtonPayment)<PaymentTypeProps>`
+  &:hover:not(:active) {
+    background-color: ${(props) => props.theme['base-hover']};
+  }
+
+  ${(props) =>
+    props.paymentType === 'Money' ? 'outline: 1px solid #8047F8' : 'none'};
+  ${(props) =>
+    props.paymentType === 'Money' ? 'background-color: #EBE5F9' : 'none'};
+`
